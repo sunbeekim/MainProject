9f339ba9c6ffbfae2a0ea4fa71de82653fb68756
sunbeekim <rlatjsql12@gmail.com>
2025-02-19 12:38:57 +0900

    test

--- GateWay/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ GateWay/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://sunbee.world", "http://www.sunbee.world"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
