63ebb94292b7434ba8e3663e511868ed4ceaefdb
sunbeekim <rlatjsql12@gmail.com>
2025-02-20 19:36:04 +0900

    test

--- GateWay/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ GateWay/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -16,31 +16,10 @@ public class WebSecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeExchange(auth -> auth
                 .pathMatchers("/**").permitAll()
             );
         return http.build();
     }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-            "http://localhost:3000", 
-            "http://sunbee.world",
-            "http://www.sunbee.world",
-            "https://sunbee.world",
-            "https://www.sunbee.world"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
 } 
