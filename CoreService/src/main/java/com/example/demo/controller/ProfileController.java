2b1679e6db1f2077c6c7d9868cc956b27ecb15cd
Lcjam <ijamiei779@gmail.com>
2025-03-11 11:23:14 +0900

    파일정리 및 채팅 테이블 수정

--- CoreService/src/main/java/com/example/demo/controller/ProfileController.java
+++ CoreService/src/main/java/com/example/demo/controller/ProfileController.java
@@ -18,7 +18,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.nio.file.Files;
--- CoreService/src/main/java/com/example/demo/dto/Market/ProductRequest.java
+++ CoreService/src/main/java/com/example/demo/dto/Market/ProductRequest.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
